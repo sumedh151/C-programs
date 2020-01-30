f074b2c88772d31a7e15c934670128c2f531a1f8
Sumedh Ghavat <38728611+sumedh151@users.noreply.github.com>
2020-01-30 20:23:34 +0530

    C program to find ascii value of a character

---
+++ asciivalue.c
@@ -0,0 +1,8 @@
+void main()
+{
+    char ch,ch1;
+    printf("\n Enter character: ");
+    scanf(" %c",&ch1);
+    printf("\nCharacter entered is:%c",ch1);
+    printf("\nascii value of %c is %d",ch1,ch1);
+}
